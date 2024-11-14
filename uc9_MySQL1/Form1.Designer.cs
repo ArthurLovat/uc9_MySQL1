8801fa55e198616a1a7ccdbada90e5b731a7143e
ArthurLovat <articfin.eco@gmail.com>
2024-11-13 21:27:24 -0300

    Adicionar arquivos de projeto.

---
+++ uc9_MySQL1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace uc9_MySQL1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBusca = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lsvLista = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefone";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(90, 30);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(90, 60);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(90, 94);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 5;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(115, 149);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 6;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(285, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Buscar";
+            // 
+            // txtBusca
+            // 
+            this.txtBusca.Location = new System.Drawing.Point(288, 56);
+            this.txtBusca.Name = "txtBusca";
+            this.txtBusca.Size = new System.Drawing.Size(196, 20);
+            this.txtBusca.TabIndex = 8;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(490, 53);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // lsvLista
+            // 
+            this.lsvLista.HideSelection = false;
+            this.lsvLista.Location = new System.Drawing.Point(288, 82);
+            this.lsvLista.Name = "lsvLista";
+            this.lsvLista.Size = new System.Drawing.Size(277, 90);
+            this.lsvLista.TabIndex = 10;
+            this.lsvLista.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 247);
+            this.Controls.Add(this.lsvLista);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBusca);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBusca;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.ListView lsvLista;
+    }
+}
+
---
+++ uc9_MySQL1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Icao;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace uc9_MySQL1
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection Connection;
+        private string data_source = "datasource=localhost;username=root;password=root;database=db_agenda";
+        public Form1()
+        {
+            InitializeComponent();
+
+            lsvLista.View = View.Details;
+            lsvLista.LabelEdit = true;
+            lsvLista.AllowColumnReorder = true;
+            lsvLista.GridLines = true;
+
+            lsvLista.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            lsvLista.Columns.Add("Nome", 150, HorizontalAlignment.Left);
+            lsvLista.Columns.Add("Telefone", 150, HorizontalAlignment.Left);
+
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                
+                //criar conexao MySql
+                Connection = new MySqlConnection(data_source);
+
+                //execultar comando insert
+                string sql = "INSERT INTO contato (nome, email, telefone) " +
+                "VALUES('" + txtNome.Text + "','" + txtEmail.Text + "', '" + txtTelefone.Text + "')";
+
+                //exibir o comando inserir dados
+                MessageBox.Show(sql);
+                MySqlCommand comando = new MySqlCommand(sql, Connection);
+
+                Connection.Open();
+
+                comando.ExecuteReader();
+
+                MessageBox.Show("Foi!");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: ", ex.Message);
+               
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string q = "'%" + txtBusca.Text + "%'";
+                
+                MessageBox.Show(q);
+
+                //criar conexao MySQL
+                Connection = new MySqlConnection(data_source);
+
+                string sql = "SELECT * " +
+                             "FROM contato " +
+                             "WHERE nome LIKE " + q + "OR email LIKE" + q;
+
+                Connection.Open();
+
+                MessageBox.Show(sql);
+
+                //executar comando
+                MySqlCommand comando = new MySqlCommand(sql, Connection);
+
+                MySqlDataReader reader = comando.ExecuteReader();
+                        
+                lsvLista.Items.Clear();
+
+                while (reader.Read())
+                {
+
+                    string[] row =
+                    {
+                        reader.GetValue(0).ToString(),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3)
+                    };
+
+                    var linha_listview = new ListViewItem(row);
+
+
+                    lsvLista.Items.Add(linha_listview);
+                }
+
+            MessageBox.Show("Deu certo de novo!!!");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+               
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
+    }
+}
